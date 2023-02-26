0e3d7d8407d184423b7088eac24c4c4cc9ca7628
JYSeo2 <wkdrbwkd2@gmail.com>
2023-02-26 15:48:38 +0900

    23/2/26

---
+++ augmented assginment operator.py
@@ -0,0 +1,8 @@
+# augmented assginment operator - neater way
+some_value = 5
+some_value = 5 + 2
+print(some_value)
+
+soe_value = 5
+soe_value += 7
+print(soe_value)
---
+++ expression and statements.py
@@ -0,0 +1,3 @@
+#expression and statements
+iq = 100
+user_age = iq / 5 # "iq/5" expressiion it's a piece of code that produces a value "<-- statement entire line of code, perform some sort of action = calculating and assigning" 
