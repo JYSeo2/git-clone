d37e0aa82352e37a53a20b79e6c1fd2c50206cfe
JinYoung_Seo <112146710+JYSeo2@users.noreply.github.com>
2023-05-25 00:42:43 +0900

    Create string indexs.py

---
+++ string indexs.py
@@ -0,0 +1,19 @@
+selfish = 'me me me'
+        #  01234567
+        
+print(selfish[0])
+
+# [start:stop:stepover]
+print(selfish[0:3:1])
+
+print(selfish[7:0:-1])
+
+print(selfish[-1])
+
+# print(selfish(-2:-1:1)) print(selfish(-1:-2:1)) both of case invalid
+
+print(selfish[-5:]) #start with reverse fifth letter m
+
+print(selfish[:7:]) #this don't include last one !remember
+
+print(selfish[::2]) #step over by 2
